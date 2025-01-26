f32393756c5e7edafcffb0b73a4736fa17373dc6
Peezy0 <prince7okyere@gmail.com>
2025-01-26 02:23:49 -0500

    Started work on the html,css and javascript

---
+++ script.js
@@ -0,0 +1,89 @@
+//Add your global variables here
+let play = false;
+let pattern = [];
+let level = 4;
+
+
+
+
+// Add your functions here
+const randomNums = (pMin, pMax) => {
+    let min = Math.ceil(pMin);
+    let max = Math.floor(pMax);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+const randomPattern = () => {
+    for (let i = 0; i < 7; i++) {
+        pattern.push(randomNums(0, 3));
+    }
+}
+
+const startGame = () => {
+    play = true;
+    randomPattern();
+    playHint();
+}
+
+const playHint = async () => {
+    for (let i = 0; i < level; i++) {
+        await highlightButton(pattern[i]);
+    }
+
+}
+// it adds active to the classlist and so if it has GreenS it adds active to it and highlights the button sets to whatever class
+const highlightButton = async (button) => {
+    const docButton = document.getElementById('button-' + button);
+    docButton.classList.add('active');
+    setTimeout(() => {
+        docButton.classList.remove('active');
+    }, 1000)
+
+}
+
+
+
+
+
+
+
+// Sound Synthesis Functions for Steps 6-8
+// You do not need to edit the below code
+const freqMap = {
+    1: 261.6,
+    2: 329.6,
+    3: 392,
+    4: 466.2
+}
+function playTone(btn, len) {
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025)
+    context.resume()
+    tonePlaying = true
+    setTimeout(function () {
+        stopTone()
+    }, len)
+}
+function startTone(btn) {
+    if (!tonePlaying) {
+        context.resume()
+        o.frequency.value = freqMap[btn]
+        g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025)
+        context.resume()
+        tonePlaying = true
+    }
+}
+function stopTone() {
+    g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025)
+    tonePlaying = false
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+let AudioContext = window.AudioContext || window.webkitAudioContext
+let context = new AudioContext()
+let o = context.createOscillator()
+let g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0, context.currentTime)
+o.connect(g)
+o.start(0)
