deed44e422af06117b405dd10d4ea36d31bfd5a3
Peezy0 <prince7okyere@gmail.com>
2025-01-26 18:51:01 -0500

    comment update for the javascript code

--- script.js
+++ script.js
@@ -7,30 +7,34 @@ let level = 4;
 
 
 // Add your functions here
+//Random integer function
 const randomNums = (pMin, pMax) => {
     let min = Math.ceil(pMin);
     let max = Math.floor(pMax);
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+// Function to add 8 random numbers to the pattern array to get a random pattern for the sound hint
 const randomPattern = () => {
     for (let i = 0; i < 7; i++) {
         pattern.push(randomNums(0, 3));
     }
 }
-
+// Function to start the game, create the pattern, then play the sequence
 const startGame = () => {
     play = true;
     randomPattern();
     playHint();
 }
 
+//Function to show highlights to the buttons, that follows the pattern array nums (0,1,2,3)
 const playHint = async () => {
     for (let i = 0; i < level; i++) {
         await highlightButton(pattern[i]);
     }
 
 }
-// it adds active to the classlist and so if it has GreenS it adds active to it and highlights the button sets to whatever class
+// Function that adds active to the classlist and so if it has GreenS it adds active to it and highlights the button sets to whatever class
 const highlightButton = async (button) => {
     const docButton = document.getElementById('button-' + button);
     docButton.classList.add('active');
